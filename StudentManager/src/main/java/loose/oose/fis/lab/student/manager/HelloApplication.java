74409b6d2dc558f881fad2d3df225f134195e1be
johhny30k <johhny.k7@gmail.com>
2023-03-22 17:15:28 +0200

    SM-2 Cleaned up the code by deleting unnecessary files

--- StudentManager/src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
+++ StudentManager/src/main/java/loose/oose/fis/lab/student/manager/HelloApplication.java
@@ -9,11 +9,12 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
-        primaryStage.setTitle("FIS - Student Manager");
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        primaryStage.setTitle("Student Manager");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
